cf8e7e12e640cf7d19718ca06812e3b49c74bfa2
DESKTOP-D86DLFV\Patrick Alvares <patrickalvares@outlook.com>
2023-06-19 20:04:13 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/responsive/mobile_screen_layout.dart';
+import 'package:flutter_instagram_clone/responsive/responsive_layout_screen.dart';
+import 'package:flutter_instagram_clone/responsive/web_screen_layout.dart';
+import 'package:flutter_instagram_clone/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Instagram Clone',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark()
+            .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+        home: const ResponsiveLayout(
+            webScreenLayout: webScreenLayout(),
+            mobileScreenLayout: mobileScreenLayout()));
+  }
+}
---
+++ lib/responsive/mobile_screen_layout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class mobileScreenLayout extends StatelessWidget {
+  const mobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Esse é Mobile')));
+  }
+}
