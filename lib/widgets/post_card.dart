95f15d4b45b0e9270bfc32959a66d6734d387d18
Notebook Patrick <patrickalvares@outlook.com>
2023-06-30 11:15:59 -0300

    Feed Posts UI final

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -45,7 +45,7 @@ class PostCard extends StatelessWidget {
                         child: ListView(
                           padding: const EdgeInsets.symmetric(vertical: 16),
                           shrinkWrap: true,
-                          children: const [Text('Delete')]
+                          children: const ['Apagar']
                               .map((e) => InkWell(
                                     onTap: () {},
                                     child: Container(
@@ -60,6 +60,103 @@ class PostCard extends StatelessWidget {
                     );
                   },
                   icon: const Icon(Icons.more_vert))
+            ],
+          ),
+        ),
+        //IMAGE SECTION
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.35,
+          width: double.infinity,
+          child: Image.network(
+            'https://www.cvc.com.br/dicas-de-viagem/wp-content/uploads/2022/06/shutterstock_736022743.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+        //LIKE COMMENT SECTION
+        Row(
+          children: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.comment_outlined,
+                )),
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.send,
+                )),
+            Expanded(
+                child: Align(
+              alignment: Alignment.bottomRight,
+              child: IconButton(
+                  onPressed: () {}, icon: const Icon(Icons.bookmark_border)),
+            ))
+          ],
+        ),
+        //DESCRIPTION AND COMMENTS
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              DefaultTextStyle(
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle2!
+                    .copyWith(fontWeight: FontWeight.w800),
+                child: Text(
+                  '1.231 Curtidas',
+                  style: Theme.of(context).textTheme.bodyText2,
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.only(top: 8),
+                child: RichText(
+                  text: const TextSpan(
+                      style: TextStyle(color: Colors.white),
+                      children: [
+                        TextSpan(
+                          text: 'username',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: ' ',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: 'Hey this is some description to be replaced',
+                        ),
+                      ]),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: const Text(
+                    'Ver todos os comentarios',
+                    style: TextStyle(fontSize: 16, color: secondaryColor),
+                  ),
+                ),
+              ),
+              InkWell(
+                onTap: () {},
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 4),
+                  child: const Text(
+                    '22/12/2021',
+                    style: TextStyle(fontSize: 16, color: secondaryColor),
+                  ),
+                ),
+              ),
             ],
           ),
         )
